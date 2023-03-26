1a41bce8ac7af50cceec66cbcee0434e363ee825
Aisha Hussein <aishasalaam07@gmail.com>
2023-03-26 18:09:03 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const createTaskForm = document.getElementById('create-task-form');
+  const input = document.getElementById('new-task-description');
+  const btn = document.getElementsByClassName('btn');
+  createTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // your code here
+    buildList(input.value);
+    input.value = "";
+  });
+  
+  const buildList = (todo) => {
+    let li = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.style.backgroundColor = 'red'
+    btn.addEventListener('click', deleteHandler)
+    btn.textContent = 'x'
+    li.textContent = `${todo} `
+    li.appendChild(btn)
+    console.log(li)
+    document.querySelector('#tasks').appendChild(li)
+  }
+  function deleteHandler(e) {
+    e.target.parentNode.remove()
+  }
+  function sortList(e) {
+    e.target.parentNode.sort()
+  }
+  
+  
 });
